b72d5512ec97c022a9fba575307c01b3681e1630
kellymacdonald <65794915+kellymacdonald@users.noreply.github.com>
2022-12-12 22:46:41 -0500

    Update integrated-test.py

--- integrated-test.py
+++ integrated-test.py
@@ -1,67 +1,105 @@
 import time 
 import RPi.GPIO as GPIO
-
-# This code snippet is for Version 1.2 
-
-# import the library
 from RpiMotorLib import RpiMotorLib
-
-
+from ina219 import INA219
+from ina219 import DeviceRangeError
 def degToStep(degrees):
     #7.2 degrees per step sequence
     steps = degrees/7.2
     return steps
-
+#  Pin initialization
+# lead screws
 GpioPinsIndex = [18, 23, 24, 25]
-GpioPins = [17, 27, 22, 10]
-
-# Declare an named instance of class pass a name and type of motor
-mymotortest = RpiMotorLib.BYJMotor("MyMotorOne", "Nema")
+GpioPinsLead = [17, 27, 22, 10]
+leadMotor = RpiMotorLib.BYJMotor("Lead", "Nema")
 indexMotor = RpiMotorLib.BYJMotor("Index", "Nema")
 time.sleep(0.5)
-
-# call the function pass the parameters
-numDegrees = 2100
-# numDegrees = 280
-#17,27, 22, 10
-#True is counterclockwise
-#True is down
-mymotortest.motor_run(GpioPins , 0.001, degToStep(numDegrees), True, False, "half", .05)
-
-import time 
-import RPi.GPIO as GPIO
-
-# This code snippet is for Version 1.2 
-
-# import the library
-from RpiMotorLib import RpiMotorLib
-
-
-def degToStep(degrees):
-    #7.2 degrees per step sequence
-    steps = degrees/7.2
-    return steps
-
-# GpioPins = [18, 23, 24, 25]
-GpioPins = [17, 27, 22, 10]
-
-# Declare an named instance of class pass a name and type of motor
-mymotortest = RpiMotorLib.BYJMotor("MyMotorOne", "Nema")
-time.sleep(0.5)
-
-# call the function pass the parameters
-numDegrees = 2100
-# numDegrees = 280
-#17,27, 22, 10
-#True is counterclockwise
-#True is down
-mymotortest.motor_run(GpioPins , 0.001, degToStep(numDegrees), True, False, "half", .05)
-
-# # state 4 --> state 1 (270 degree cw turn)
+# DC motor
+in1 = 14
+in2 = 15
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(in1, GPIO.OUT)
+GPIO.setup(in2, GPIO.OUT)
+#set both motor pins low
+GPIO.output(in1,GPIO.LOW)
+GPIO.output(in2,GPIO.LOW)
+# # state 1 --> state 4 (270 degree ccw turn)
 index1to4 = 275
-indexMotor.motor_run(GpioPinsIndex , 0.01, degToStep(index1to4), False, False, "half", .05)
+indexMotor.motor_run(GpioPinsIndex , 0.01, degToStep(index1to4), True, False, "half", .05)
+#stall stepper
+GPIO.output(18,GPIO.HIGH)
+GPIO.output(23,GPIO.HIGH)
+# move lead screw up for capping
+leadStroke = 2100
+leadMotor.motor_run(GpioPinsLead , 0.001, degToStep(leadStroke), False, False, "half", .05)
+# activate capping motor and check current levels
+# turn DC motor on
+GPIO.output(in2,GPIO.HIGH)
+SHUNT_OHMS = 0.1
+ina = INA219(SHUNT_OHMS, busnum=1)
+ina.configure()
+threshold = -500
+index = 0
+above_count = 0
+while True:
+    try:
+        current_level = ina.current()
+        print("Bus Current: %.3f mA" % current_level)
+        # print(current_level, "mA")
+        if current_level < threshold and index > 200:
+            above_count +=1
+            print("reached threshold")
+        if above_count > 3:
+            break
+    except DeviceRangeError as e:
+        # Current out of device range with specified shunt resistor
+        print(e)
+    index +=1
+# turn off capping motor
+GPIO.output(in2,GPIO.LOW)
+# move lead screw down
+leadMotor.motor_run(GpioPinsLead , 0.001, degToStep(leadStroke), True, False, "half", .05)
+# unlock stepper motor
+GPIO.output(17,GPIO.LOW)
+GPIO.output(27,GPIO.LOW)
+# Move from state 4 --> state 2 (180 cw turn)
+index4to2 = 180
+indexMotor.motor_run(GpioPinsIndex , 0.01, degToStep(index4to2), False, False, "half", .05)
+# from time import sleep  
+# GPIO.setmode(GPIO.BOARD)  
+# This is for the kicker
+print("beginning of kicker code")
+GPIO.setup(20,GPIO.OUT)  
+pwm=GPIO.PWM(20,50)  
+pwm.start(0)  
+print("starting kicker for loop")
+for i in range(0,181):
+    sig=(i/18)+2  
+    pwm.ChangeDutyCycle(sig)  
+    print("here")
+    time.sleep(0.01)  
+for i in range(180,-1,-1):  
+    sig=(i/18)+2  
+    pwm.ChangeDutyCycle(sig)  
+    time.sleep(0.01)  
+pwm.stop()  
+# Move from state 2 --> state 1 (90 cw turn)
+index2to1 = 90
+indexMotor.motor_run(GpioPinsIndex , 0.01, degToStep(index2to1), False, False, "half", .05)
 
-#stall indexing stepper
+#stall stepper
 GPIO.output(18,GPIO.HIGH)
 GPIO.output(23,GPIO.HIGH)
 
+
+#what does it need to be able to do
+
+# start at state 1
+# move 270 to state 4
+# stall stepper motor so that it locks against wall
+# move lead screw up for capping
+# activate capping motor -- check current
+# unlock stepper motor
+# move lead screw down
+# move to state (180 cw)
+# move to state (180 cw)
